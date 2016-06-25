75b5dae158959511f6fa784b98ccf3ac11ee796b
Yak Jun Xiang <snowypowers@gmail.com>
2016-06-25 17:30:32 +0800

    add button to go back to main menu from sampleGame

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+public void nextScene(string scene){
+		UnityEngine.SceneManagement.SceneManager.LoadScene(scene);
+	}
+}
