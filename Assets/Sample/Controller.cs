7114a85bd514f769dcaf17dcbc67598a051f2308
Yak Jun Xiang <snowypowers@gmail.com>
2016-06-26 12:20:01 +0800

    Add reset button to sampleGame

--- Assets/Sample/Controller.cs
+++ Assets/Sample/Controller.cs
@@ -5,38 +5,53 @@ using System.Collections;
 public class Controller : MonoBehaviour {
 
 	private Score data;
-	public Text highScoreText;
-	public Text Scoretext;
 	public int startTime;
-    public Text Timetext;
-    public Button b;
+	private Text highScoreText;
+	private Text Scoretext;
+    private Text Timetext;
+    private Button scoreButton;
+	private Button resetButton;
 
+	private int timeLeft;
     private float secondCount;
     private bool end = false;
 
 	void Start() {
+		//Find all UI elements
+		highScoreText = GameObject.Find("High Score Text").GetComponent<Text>();
+		Scoretext = GameObject.Find("Score Text").GetComponent<Text>();	
+		Timetext = GameObject.Find("Timer Text").GetComponent<Text>();
+		scoreButton = GameObject.Find("ScoreButton").GetComponent<Button>();
+		resetButton = GameObject.Find("Reset Button").GetComponent<Button>();
+
 		//Init data and load
 		data = new Score();
 		data.LoadScore();
 
+		//Init timeLeft
+		timeLeft = startTime;
 		//Init all text
 		Scoretext.text = "Score:\n" + data.score.ToString();
 		highScoreText.text = "High Score:\n" + data.highScore.ToString();
-		Timetext.text = "Time Left: \n" + startTime.ToString();
+		Timetext.text = "Time Left: \n" + timeLeft.ToString();
+
+		//Hide reset Button
+		resetButton.gameObject.SetActive(false);
 	}
 
 	void Update () {
 		//Update timer
         secondCount += Time.deltaTime;
-        if (secondCount > 1.0f && startTime > 0)
+        if (secondCount > 1.0f && timeLeft > 0)
         {
-            startTime -= 1;
+            timeLeft -= 1;
             secondCount -= 1.0f;
+			Timetext.text = "Time Left: \n" + timeLeft.ToString();
         }
-        Timetext.text = "Time Left: \n" + startTime.ToString();
+        
 
 		//End game if timer reaches 0
-        if (startTime == 0 && end == false) {
+        if (timeLeft == 0 && end == false) {
             endGame();
             end = true;
 
@@ -63,11 +78,25 @@ public class Controller : MonoBehaviour {
 		if (data.score > data.highScore) {
 			data.highScore = data.score;
 			updateScore();
+			Timetext.text = "New High Score!";
 			SaveLoad.Save();
 		}
 	}
 	void endGame() {
         saveScore();
-    	b.interactable = false;
+    	scoreButton.enabled = false;
+		resetButton.gameObject.SetActive(true);
     }
+
+	public void resetGame() {
+		data.score = 0;
+		timeLeft = startTime;
+		secondCount = 0;
+		end = false;
+		Timetext.text = "Time Left: \n" + timeLeft.ToString();
+		updateScore();
+		scoreButton.enabled = true;
+		resetButton.gameObject.SetActive(false);
+
+	}
 }
