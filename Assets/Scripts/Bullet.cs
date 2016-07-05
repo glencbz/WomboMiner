28534d9d0c19ea5f717e8047319d017e9b735aeb
glencbz <glencbz@gmail.com>
2016-07-05 10:01:59 +0800

    created base classes player, item, weapon, bullet

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+abstract public class Bullet : MonoBehaviour {
+
+	private Rigidbody2D rigidBody;
+	private Collider2D collider2D;
+
+	abstract protected void FireBehaviour();
+	abstract protected void OnTriggerEnter2D();
+
+	virtual protected void Start () {
+		rigidBody = GetComponent<Rigidbody2D>();
+
+		collider2D = GetComponent<Collider2D>();
+		collider2D.isTrigger = true;
+	}
+		
+	void FixedUpdate(){
+		FireBehaviour();
+	}
+
+	void Update () {
+	
+	}
+
+	public virtual void InitialFire(Vector2 initialDirection){
+		rigidBody.AddForce(initialDirection.normalized);
+	}
+}
