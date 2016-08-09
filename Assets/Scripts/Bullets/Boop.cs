5bab2250793cedcb8c4afc03614379f9947625e9
glencbz <glencbz@gmail.com>
2016-08-09 22:10:54 +0800

    basic boop

---
+++ Assets/Scripts/Bullets/Boop.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boop : Bullet {
+
+	public float pushback = 1000f;
+	public float duration = .5f;
+
+	void Start(){
+		Invoke("DestroySelf", duration);
+	}
+
+	public override void InitialFire(Transform parent, Vector3 mousePos){
+		speed = 0;
+	}
+
+	protected virtual void OnTriggerEnter2D(Collider2D other){
+		//Environment Resolution
+		Debug.Log(other.gameObject);
+		switch(other.tag) {
+			case "Player":
+			case "Enemy":
+				if (other.tag != source) {
+					Creature otherCreature = other.GetComponent<Creature>();
+					Debug.Log(otherCreature);
+					Vector3 forceDirection = (otherCreature.transform.position - transform.position).normalized;
+					Debug.Log(forceDirection);
+					otherCreature.GetComponent<Rigidbody2D>().AddForce(forceDirection * pushback);
+					otherCreature.takeDamage(damage);
+	//				Destroy(gameObject);
+				}
+				break;
+		}
+	}
+
+	protected void DestroySelf(){
+		Destroy(gameObject);
+	}
+}
