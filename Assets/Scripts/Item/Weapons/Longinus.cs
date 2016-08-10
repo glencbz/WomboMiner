b77b1d6da7fb0f817c16556699b108219307d8cc
Yak Jun Xiang <snowypowers@gmail.com>
2016-08-11 04:07:21 +0800

    Longinus Spear and Larger Red Bullet

---
+++ Assets/Scripts/Item/Weapons/Longinus.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Longinus : Weapon {
+
+	
+	protected override void Update () {
+		//Update cooldown
+		if (cooldownStatus > 0) {
+			cooldownStatus -= Time.deltaTime;
+			if (cooldownStatus < 0) { 
+				cooldownStatus = 0;
+				sr.enabled = true;
+				Debug.Log(transform.parent.name);
+				transform.parent.gameObject.GetComponent<SpriteRenderer>().enabled = false;
+			}
+		}
+	}
+	public override void FireBullet(Vector3 direction) {
+		if (cooldownStatus <= 0){
+			cooldownStatus = cooldown;
+			GenerateBullet(direction);
+			sr.enabled = false;
+			transform.parent.gameObject.GetComponent<SpriteRenderer>().enabled = true;
+		}
+	}
+}
